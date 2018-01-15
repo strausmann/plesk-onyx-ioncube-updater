9bb7905dcd44566e334136e8fea28bec4a2db5b8
Bjoern Strausmann <bjoern@strausmann.net>
2018-01-15 15:12:49 +0100

    Fix Service Namen für den Restart der FPM PHP Services

--- update_ioncube.sh
+++ update_ioncube.sh
@@ -13,7 +13,7 @@ if [ -d "/opt/plesk/php/5.4/" ]
 then
     echo "Installation des ionCube Loader für PHP 5.4"
     sudo cp /usr/local/ioncube/ioncube_loader_lin_5.4.so /opt/plesk/php/5.4/lib/php/modules/
-    sudo service plesk-php54-fpm.service restart
+    sudo service plesk-php54-fpm restart
 fi
 
 ### PHP 5.5
@@ -21,7 +21,7 @@ if [ -d "/opt/plesk/php/5.5/" ]
 then
     echo "Installation des ionCube Loader für PHP 5.5"
     sudo cp /usr/local/ioncube/ioncube_loader_lin_5.5.so /opt/plesk/php/5.5/lib/php/modules/
-    sudo service plesk-php55-fpm.service restart
+    sudo service plesk-php55-fpm restart
 fi
 
 ### PHP 5.6
@@ -29,7 +29,7 @@ if [ -d "/opt/plesk/php/5.6/" ]
 then
     echo "Installation des ionCube Loader für PHP 5.6"
     sudo cp /usr/local/ioncube/ioncube_loader_lin_5.6.so /opt/plesk/php/5.6/lib/php/modules/
-    sudo service plesk-php56-fpm.service restart
+    sudo service plesk-php56-fpm restart
 fi
 
 ### PHP 7.0
@@ -37,7 +37,7 @@ if [ -d "/opt/plesk/php/7.0/" ]
 then
     echo "Installation des ionCube Loader für PHP 7.0"
     sudo cp /usr/local/ioncube/ioncube_loader_lin_7.0.so /opt/plesk/php/7.0/lib/php/modules/
-    sudo service plesk-php70-fpm.service restart
+    sudo service plesk-php70-fpm restart
 fi
 
 ### PHP 7.1
@@ -45,7 +45,7 @@ if [ -d "/opt/plesk/php/7.1/" ]
 then
     echo "Installation des ionCube Loader für PHP 7.1"
     sudo cp /usr/local/ioncube/ioncube_loader_lin_7.1.so /opt/plesk/php/7.1/lib/php/modules/
-    sudo service plesk-php71-fpm.service restart
+    sudo service plesk-php71-fpm restart
 fi
 
 ### PHP 7.2
@@ -53,7 +53,7 @@ if [ -d "/opt/plesk/php/7.2/" ]
 then
     echo "Installation des ionCube Loader für PHP 7.2"
     sudo cp /usr/local/ioncube/ioncube_loader_lin_7.2.so /opt/plesk/php/7.2/lib/php/modules/
-    sudo service plesk-php72-fpm.service restart
+    sudo service plesk-php72-fpm restart
 fi
 
 echo "Remove TEMP Files"
