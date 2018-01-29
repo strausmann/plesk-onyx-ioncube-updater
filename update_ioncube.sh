45d2b69be09f00bcd0ad4e0e6b0ddf0927b66f49
Bjoern Strausmann <bjoern@strausmann.net>
2018-01-29 20:24:52 +0100

    Update update_ioncube.sh

--- update_ioncube.sh
+++ update_ioncube.sh
@@ -12,7 +12,7 @@ sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz
 if [ -d "/opt/plesk/php/5.4/" ] 
 then
     echo "Installation des ionCube Loader für PHP 5.4"
-    sudo cp /usr/local/ioncube/ioncube_loader_lin_5.4.so /opt/plesk/php/5.4/lib/php/modules/
+    sudo cp /usr/local/ioncube/ioncube_loader_lin_5.4.so /opt/plesk/php/5.4/lib64/php/modules/
     sudo service plesk-php54-fpm restart
 fi
 
@@ -20,7 +20,7 @@ fi
 if [ -d "/opt/plesk/php/5.5/" ] 
 then
     echo "Installation des ionCube Loader für PHP 5.5"
-    sudo cp /usr/local/ioncube/ioncube_loader_lin_5.5.so /opt/plesk/php/5.5/lib/php/modules/
+    sudo cp /usr/local/ioncube/ioncube_loader_lin_5.5.so /opt/plesk/php/5.5/lib64/php/modules/
     sudo service plesk-php55-fpm restart
 fi
 
@@ -28,7 +28,7 @@ fi
 if [ -d "/opt/plesk/php/5.6/" ] 
 then
     echo "Installation des ionCube Loader für PHP 5.6"
-    sudo cp /usr/local/ioncube/ioncube_loader_lin_5.6.so /opt/plesk/php/5.6/lib/php/modules/
+    sudo cp /usr/local/ioncube/ioncube_loader_lin_5.6.so /opt/plesk/php/5.6/lib64/php/modules/
     sudo service plesk-php56-fpm restart
 fi
 
@@ -36,7 +36,7 @@ fi
 if [ -d "/opt/plesk/php/7.0/" ] 
 then
     echo "Installation des ionCube Loader für PHP 7.0"
-    sudo cp /usr/local/ioncube/ioncube_loader_lin_7.0.so /opt/plesk/php/7.0/lib/php/modules/
+    sudo cp /usr/local/ioncube/ioncube_loader_lin_7.0.so /opt/plesk/php/7.0/lib64/php/modules/
     sudo service plesk-php70-fpm restart
 fi
 
@@ -44,7 +44,7 @@ fi
 if [ -d "/opt/plesk/php/7.1/" ] 
 then
     echo "Installation des ionCube Loader für PHP 7.1"
-    sudo cp /usr/local/ioncube/ioncube_loader_lin_7.1.so /opt/plesk/php/7.1/lib/php/modules/
+    sudo cp /usr/local/ioncube/ioncube_loader_lin_7.1.so /opt/plesk/php/7.1/lib64/php/modules/
     sudo service plesk-php71-fpm restart
 fi
 
@@ -52,7 +52,7 @@ fi
 if [ -d "/opt/plesk/php/7.2/" ] 
 then
     echo "Installation des ionCube Loader für PHP 7.2"
-    sudo cp /usr/local/ioncube/ioncube_loader_lin_7.2.so /opt/plesk/php/7.2/lib/php/modules/
+    sudo cp /usr/local/ioncube/ioncube_loader_lin_7.2.so /opt/plesk/php/7.2/lib64/php/modules/
     sudo service plesk-php72-fpm restart
 fi
 
